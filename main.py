0dc2ed1bae5b04a8d02f10c5a257b5c4c7fe66e0
AbbyH2025 <william.hendrix2025@gbwl.org>
2024-09-10 19:09:53 +0000

    added vars in main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     print("hello world")
 
+    #declare vaiables
+    age = 17
+    teacher = "navab"
+    is_teacher = True
+    my_grade = 99.99
+    
 
 if __name__ == "__main__":
     main()
